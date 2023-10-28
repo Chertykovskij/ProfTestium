0589f23373373a8ec873c9c2ed2ff3ba547648b8
chertykovskij <chertykovskij@gmail.com>
2023-10-28 12:43:28 +0500

    Добавлен личный кабинет

---
+++ Controllers/PersonalAccount.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProfTestium.Controllers
+{
+    public class PersonalAccount : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
