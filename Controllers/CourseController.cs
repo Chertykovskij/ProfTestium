0bdeabe58c75ee2ca3e226fd331bd2573376ccc1
chertykovskij <chertykovskij@gmail.com>
2023-10-29 17:51:50 +0500

    Просмотр списка курсов

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProfTestium.Models;
+using ProfTestium.Models.Contexts;
+
+namespace ProfTestium.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ProfTestiumContext _context;
+
+        public CourseController(ProfTestiumContext context) { _context = context; }
+
+        public ActionResult Index()
+        {
+            List<Course> courses = _context.Courses.ToList();
+
+            return View(courses);
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
