528e18bb368b62ecdece32d3bc7845a821d08211
Щелков Алексей Иванович <orenburgstyle@mail.ru>
2023-10-29 20:40:28 +0500

    ашчfix

---
+++ Migrations/20231029153449_new1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProfTestium.Migrations
+{
+    public partial class new1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NameAdministrator",
+                table: "Organizations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NameAdministrator",
+                table: "Organizations");
+        }
+    }
+}
